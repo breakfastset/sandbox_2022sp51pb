afe5f67bf28d41bcc418e2d085d40ef3d888b2cd
breakfastset <kleng99@gmail.com>
2022-04-01 09:31:39 +0800

    Refactor email format checking into a function

--- email_checker.py
+++ email_checker.py
@@ -2,10 +2,15 @@
 
 def main():
     email = "adam_sandler@yahoo.com"
-    if "@" in email and "." in email:
+    if valid_email_format(email):
         print("Valid email format")
     else:
         print("Invalid email format")
+
+
+def valid_email_format(email):
+    """Return true if email contains @ and ."""
+    return "@" in email and "." in email
 
 
 if __name__ == '__main__':
